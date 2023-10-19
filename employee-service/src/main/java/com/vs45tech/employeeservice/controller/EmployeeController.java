094e32f390884d13990e463a600538a5a1b94dfb
vs45 <bringfirehjake88@gmail.com>
2023-10-19 15:41:48 +0100

    employee-service added

---
+++ employee-service/src/main/java/com/vs45tech/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,45 @@
+package com.vs45tech.employeeservice.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vs45tech.employeeservice.model.Employee;
+import com.vs45tech.employeeservice.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    
+     private static final Logger LOGGER=LoggerFactory.getLogger(EmployeeController.class);
+    @Autowired
+    private EmployeeRepository employeeRepository;
+     public Employee addEmployee(@RequestBody Employee employee){
+        LOGGER.info("Department add: {}",employee); 
+return employeeRepository.addEmployee(employee);
+    }
+    @GetMapping
+    public List<Employee> getAll(){
+         LOGGER.info("Department find: {}"); 
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee findById(@PathVariable Long id){
+         LOGGER.info("Employee find: id={}",id); 
+        return employeeRepository.findById(id);
+    }
+
+    @GetMapping("/department/{deptId}")
+    public List<Employee> findByDepartment(@PathVariable("deptId") Long deptId){
+       return employeeRepository.findByDepartment(deptId); 
+    }
+
+}
---
+++ employee-service/src/main/java/com/vs45tech/employeeservice/repository/EmployeeRepository.java
@@ -0,0 +1,30 @@
+package com.vs45tech.employeeservice.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.vs45tech.employeeservice.model.Employee;
+
+@Repository
+public class EmployeeRepository{
+    private List<Employee> employees=new ArrayList<>();
+
+    public Employee addEmployee(Employee employee){
+        employees.add(employee);
+        return employee;
+    }
+
+    public Employee findById(Long id){
+        return employees.stream()
+        .filter(e->e.id().equals(id)).findFirst().orElseThrow();
+    }
+    public List<Employee> findAll(){
+        return employees; 
+}
+ public List<Employee> findByDepartment(Long departmentId){
+        return employees.stream()
+        .filter(e->e.departmentId().equals(departmentId)).toList();
+    }
+}
